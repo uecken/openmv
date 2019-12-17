031fb2c045e31fe0328289dc085cac6009215c1e
kentugi <the.full.of.moon@gmail.com>
2019-12-18 01:46:50 +0900

    Update nn_cifar10.py
    Changed to detect only cat.

--- scripts/examples/25-Machine-Learning/nn_cifar10.py
+++ scripts/examples/25-Machine-Learning/nn_cifar10.py
@@ -2,7 +2,7 @@
 import sensor, image, time, os, nn
 
 sensor.reset()                         # Reset and initialize the sensor.
-sensor.set_contrast(3)
+sensor.set_contrast(2)
 sensor.set_pixformat(sensor.RGB565)    # Set pixel format to RGB565
 sensor.set_framesize(sensor.QVGA)      # Set frame size to QVGA (320x240)
 sensor.set_windowing((128, 128))       # Set 128x128 window.
@@ -11,9 +11,9 @@ sensor.set_auto_gain(False)
 sensor.set_auto_exposure(False)
 
 # Load cifar10 network
-net = nn.load('/cifar10.network')
+#net = nn.load('/cifar10.network')
 # Faster, smaller and less accurate.
-#net = nn.load('/cifar10_fast.network')
+net = nn.load('/cifar10_fast.network')
 labels = ['airplane', 'automobile', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
 
 clock = time.clock()                # Create a clock object to track the FPS.
@@ -23,10 +23,11 @@ while(True):
     out = net.forward(img)
     max_idx = out.index(max(out))
     score = int(out[max_idx]*100)
-    if (score < 70):
+    if (score < 60):
         score_str = "??:??%"
     else:
-        score_str = "%s:%d%% "%(labels[max_idx], score)
+        if (labels[max_idx] == "cat"):
+           score_str = "%s:%d%% "%(labels[max_idx], score)
     img.draw_string(0, 0, score_str, color=(255, 0, 0))
 
     print(clock.fps())             # Note: OpenMV Cam runs about half as fast when connected
